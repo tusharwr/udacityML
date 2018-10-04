77db4b652d5d54e1e73587cd74f75d1557c4cbd3
Tushar Singh <tshrsngh@gmail.com>
2018-10-05 00:54:30 +0530

    kNN algo

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -30,8 +30,36 @@ plt.show()
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.neighbors import KNeighborsClassifier
+from time import time
+from sklearn.metrics import accuracy_score
 
+#print(len(features_test))
 
+neigh = KNeighborsClassifier()
+
+t0 = time()
+neigh.fit(features_train, labels_train) 
+print("training time:", round(time()-t0, 3), "s")
+
+t1 = time()
+pred = neigh.predict(features_test)
+print("testing time:", round(time()-t1, 3), "s")
+
+accuracy = accuracy_score(labels_test, pred)
+print(accuracy)
+
+"""
+k=15
+('training time:', 0.001, 's')
+('testing time:', 0.003, 's')
+0.928
+
+k=default i.e. 5
+('training time:', 0.002, 's')
+('testing time:', 0.002, 's')
+0.92
+"""
 
 
 
