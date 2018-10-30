f5c8a28972ad9ce2ef1d04c11a6406811abc68ac
Tushar Singh <tshrsngh@gmail.com>
2018-10-30 23:44:06 +0530

    PCA

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -17,6 +17,7 @@ The dataset used in this example is a preprocessed excerpt of the
 
 
 print __doc__
+print "before import"
 
 from time import time
 import logging
@@ -31,10 +32,11 @@ from sklearn.metrics import confusion_matrix
 from sklearn.decomposition import RandomizedPCA
 from sklearn.svm import SVC
 
+print "after import "
 # Display progress logs on stdout
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
-
+print "log captured"
 ###############################################################################
 # Download the data, if not already on disk and load it as numpy arrays
 lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
@@ -66,7 +68,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 ###############################################################################
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
 # dataset): unsupervised feature extraction / dimensionality reduction
-n_components = 150
+n_components = 50
 
 print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
 t0 = time()
@@ -143,4 +145,12 @@ plot_gallery(X_test, prediction_titles, h, w)
 eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
 plot_gallery(eigenfaces, eigenface_titles, h, w)
 
-pl.show()
+#pl.show()
+
+
+##############################################################
+#Code to find highest PCA
+#print(pca.explained_variance_ratio_)
+
+print("First PCA : ",pca.explained_variance_ratio_[0])
+print("Second PCA : ",pca.explained_variance_ratio_[1])
